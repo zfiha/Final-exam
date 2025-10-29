130c9e9c01230f81a59d0d3eda5ca28de81a3301
zfiha <zannatulfiha8@gmail.com>
2025-10-29 08:15:02 +0600

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_year = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
